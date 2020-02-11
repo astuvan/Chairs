e3cc643d79709a84a39ce1176720b58b99c58e9b
astuvan <astuvan@gmail.com>
2020-02-11 15:31:15 +0100

    Fixet :D

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,17 @@
-f = open("reservations.csv")
+f = open("reservations")
 
-for reservation in f:
-    name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
-    print("{} will get {} chairs per person".format(name, chairs_per_person))
+for x in f:
+    name, number = x.split(",")
+    #print(f"Navn: {name} Reservasjoner: {number}")
+    #print(f"Tall: {int(number)}")
+    try:
+        chairs_per_person = round(50 / int(number))
+        if chairs_per_person > 50:
+            chairs_per_person = 50
+        print("{} will get {} chairs per person".format(name, chairs_per_person))
+    except ValueError as error:
+        print(f"Feil: Tall kan ikke skrives med bokstaver!! Todd!!")
+    except ZeroDivisionError as error:
+        print(f"Feil: Kan ikke reservere 0 stoler")
+    except:
+        print(f("Feil: WTF Todd!?"))
